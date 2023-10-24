23037dcf0fec199fc72c557983150a741eb47ec0
daniexs <lggr404@gmail.com>
2023-10-25 00:11:53 +0700

    selesai fitur Product

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const  router = express.Router()
 const userRouter = require('./userRouter')
-const ProductsRouter = require('./footballRouter')
+const ProductsRouter = require('./productsRouter')
 
 router.use('/user', userRouter)
 router.use('/products', ProductsRouter)

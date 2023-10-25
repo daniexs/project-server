0cba3130a4837dd7bf50e234c2b28b1ea757e911
daniexs <lggr404@gmail.com>
2023-10-25 22:55:12 +0700

    pembenaran uploadfile

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -4,10 +4,6 @@ const router = express.Router();
 const upload = require('../helpers/upload.helper');
 const uploadController = require('../controllers/uploadController');
  
-// router.post('/upload-single',   upload.single('file'),    uploadController.uploadSingle);
-// router.post('/upload-multiple', upload.array('files', 5), uploadController.uploadMultiple);
- 
-// /* ------------------------ upload and error handling ----------------------- */
-router.post('/single', uploadController.uploadSingleV2);
+router.post('/upload-multiple', upload.array('files', 5), uploadController.uploadMultiple)
 
 module.exports = router;

84d1cdc8b46629682e5e068aa75a663db180bc49
daniexs <lggr404@gmail.com>
2023-10-26 08:36:54 +0700

    sd

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,12 @@ const ProductsRouter = require('./productsRouter')
 const uploadRouter = require('./uploadRouter')
 const cartRouter = require('./bookmarkRouter')
 const checkoutRouter = require('./midTrans')
+const authentication = require('../middlewares/authentication')
 
 router.use('/user', userRouter)
 router.use('/products', ProductsRouter)
+
+router.use(authentication)
 router.use('/upload',uploadRouter)
 router.use('/cart',cartRouter)
 router.use('/midtrans', checkoutRouter)
